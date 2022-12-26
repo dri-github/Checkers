c66dd6705f4ac19817a22d61170f008a2e33db3c
MironovDanila <dan1la.mironov@yandex.by>
2022-12-26 21:24:48 +0300

    Шашки умеют ходить 3

--- core/core.hpp
+++ core/core.hpp
@@ -22,40 +22,67 @@ public:
 		//latter = 'd';
 
 		std::cout << "init" << std::endl;
-		
+
 		// ■ - белые
 		// 0 - Чёрные
 
 		width = 8;
 		height = 8;
 		board = new Board(width, height);
-		board->addChecker(0, 0, Checker::White);
-		board->addChecker(2, 0, Checker::White);
-		board->addChecker(4, 0, Checker::White);
-		board->addChecker(6, 0, Checker::White);
-		board->addChecker(1, 1, Checker::White);
-		board->addChecker(3, 1, Checker::White);
-		board->addChecker(5, 1, Checker::White);
-		board->addChecker(7, 1, Checker::White);
-		board->addChecker(0, 2, Checker::White);
-		board->addChecker(2, 2, Checker::White);
-		board->addChecker(4, 2, Checker::White);
-		board->addChecker(6, 2, Checker::White);
+		// board->addChecker(0, 0, Checker::White);
+		// board->addChecker(2, 0, Checker::White);
+		// board->addChecker(4, 0, Checker::White);
+		// board->addChecker(6, 0, Checker::White);
+		// board->addChecker(1, 1, Checker::White);
+		// board->addChecker(3, 1, Checker::White);
+		// board->addChecker(5, 1, Checker::White);
+		// board->addChecker(7, 1, Checker::White);
+		// board->addChecker(0, 2, Checker::White);
+		// board->addChecker(2, 2, Checker::White);
+		// board->addChecker(4, 2, Checker::White);
+		// board->addChecker(6, 2, Checker::White);
+		// 
+		// board->addChecker(1, 5, Checker::Black);
+		// board->addChecker(3, 5, Checker::Black);
+		// board->addChecker(5, 5, Checker::Black);
+		// board->addChecker(7, 5, Checker::Black);
+		// board->addChecker(0, 6, Checker::Black);
+		// board->addChecker(2, 6, Checker::Black);
+		// board->addChecker(4, 6, Checker::Black);
+		// board->addChecker(6, 6, Checker::Black);
+		// board->addChecker(1, 7, Checker::Black);
+		// board->addChecker(3, 7, Checker::Black);
+		// board->addChecker(5, 7, Checker::Black);
+		// board->addChecker(7, 7, Checker::Black);
 
-		board->addChecker(1, 5, Checker::Black);
-		board->addChecker(3, 5, Checker::Black);
-		board->addChecker(5, 5, Checker::Black);
-		board->addChecker(7, 5, Checker::Black);
-		board->addChecker(0, 6, Checker::Black);
-		board->addChecker(2, 6, Checker::Black);
-		board->addChecker(4, 6, Checker::Black);
-		board->addChecker(6, 6, Checker::Black);
-		board->addChecker(1, 7, Checker::Black);
-		board->addChecker(3, 7, Checker::Black);
-		board->addChecker(5, 7, Checker::Black);
-		board->addChecker(7, 7, Checker::Black);
+		board->addChecker(2, 2, Checker::Black);
+		board->addChecker(3, 3, Checker::White);
 
 		//board->getCheckerByPosition(1, 1)->status = Checker::Queen;
+
+		while (true)
+		{
+			system("cls");
+			std::cout << "======================- Checkers -======================" << std::endl;
+			std::cout << "======================-   Menu   -======================" << std::endl;
+			std::cout << "|  s - Start game                                      |" << std::endl;
+			std::cout << "|  q - Quite program                                   |" << std::endl;
+			std::cout << "========================================================" << std::endl;
+
+			unsigned char command;
+			std::cin >> command;
+			switch (command)
+			{
+			case 's':
+				return true;
+			case 'q':
+				return false;
+			default:
+				std::cout << "Not have message" << std::endl;
+				alert("Pressed eny key");
+				break;
+			}
+		}
 
 		return true;
 	}
@@ -86,20 +113,20 @@ public:
 			// 
 			// Y
 
-																					
 
-			int x1;
-			int y1;
+
+			char x1;
+			char y1;
 			int x2;
 			int y2;
-			
+
 			int l = -1;
 			int r = 1;
 
 
 			if (side == Checker::White) {
 				side = Checker::Black;
-				std::cout << "---> Black" << std::endl;
+				std::cout << " ---> black " << std::endl;
 			}
 			else {
 				side = Checker::White;
@@ -108,16 +135,22 @@ public:
 
 
 
-restart_move:
+		restart_move:
+			//Ввод координат шашки и проверка на корректность введённых данных
 			std::cout << "Coords:" << std::endl;
 			std::cin >> y1 >> x1;
-			std::cout << "Left(l) or Right(r)? ";
+			y1 = y1 - '0';
+			x1 = x1 - '0';
+			if (!(x1 >= 0 && x1 <= 7 && y1 >= 0 && y1 <= 7))
+				goto restart_move;
+			//Куда ходить шашке, налево или направо?
+			std::cout << "Left(l) or Right(r)?" << std::endl;
 			std::cin >> commandX;
 			if (commandX == 'l')
-				x2 =  l;
+				x2 = l;
 			else if (commandX == 'r')
-				x2 =  r;
-			
+				x2 = r;
+
 			Checker* checker = board->getCheckerByPosition(x1, y1);
 			switch (side)
 			{
@@ -148,6 +181,8 @@ restart_move:
 		else if (command == 's') {
 			int x1;
 			int y1;
+
+
 
 			int l = -1;
 			int r = 1;
@@ -193,7 +228,14 @@ restart_move:
 							break;
 						}
 					}
-									}
+					else
+					{
+						if ((x + y) % 2 != 0)
+							std::cout << char(0xDB);
+						else
+							std::cout << char(32);//0XB0
+					}
+				}
 			}
 			std::cout << std::endl;
 		}
@@ -204,10 +246,36 @@ restart_move:
 	//Закрывает программу
 	void close() { is_open = false; }
 
+	void alert(const char* text)
+	{
+		std::cout << text << std::endl;
+		unsigned char command;
+		std::cin >> command;
+		std::cout << "\r";
+	}
+	char* prompt(const char* text, int size)
+	{
+		std::cout << text << std::endl;
+		std::cout << "Yes(y) or Not(n)" << std::endl;
+		char* command = new char[size];
+		std::cin >> command;
+
+		return command;
+	}
+	bool confirm(const char* text)
+	{
+		std::cout << text << std::endl;
+		std::cout << "Yes(y) or Not(n)" << std::endl;
+		unsigned char command;
+		std::cin >> command;
+
+		return command == 'y';
+	}
+
 private:
 	Board* board;	//<-- Игровая доска
 	bool is_open;	//<-- Определяет открыта ли программа
-	
+
 	//Переменные объявляем после этого коментария (если есть необходимость создания переменных доступных по всему коду)
 	//char latter;	//<-- Например
 	int width;
